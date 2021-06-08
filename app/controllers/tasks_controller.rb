d144eb919491c28397b79b0debe2625df1f8f3fc
Gauderic Diagambana <diagambana.gauderic@gmail.com>
2021-06-08 14:27:28 +0200

    I can create a task code clean

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -7,11 +7,19 @@ class TasksController < ApplicationController
     @task = Task.find(params[:id])
   end
 
-  def done?
-    if @task.completed == true
-      puts 'This task is done'
-    else
-      puts 'This task is not done'
-    end
+  def new
+    @task = Task.new
+  end
+
+  def create
+    @task = Task.new(task_params)
+    @task.save
+    redirect_to task_path(@task)
+  end
+
+  private
+
+  def task_params
+    params.require(:task).permit(:title, :details)
   end
 end
