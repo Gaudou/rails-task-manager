c631225379666e0c08f9e9d343f545d0db15ac80
Gauderic Diagambana <diagambana.gauderic@gmail.com>
2021-06-08 15:30:22 +0200

    I can update a task

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -17,9 +17,28 @@ class TasksController < ApplicationController
     redirect_to task_path(@task)
   end
 
+  def edit
+    @task = Task.find(params[:id])
+  end
+
+  def update
+    @task = Task.find(params[:id])
+    @task.update(task_params_edit)
+    if params[:task][:completed] == "0"
+      puts "The task is not done"
+    else
+      puts "The task is done"
+    end
+    redirect_to task_path(@task)
+  end
+
   private
 
   def task_params
     params.require(:task).permit(:title, :details)
+  end
+
+  def task_params_edit
+    params.require(:task).permit(:title, :details, :completed)
   end
 end
