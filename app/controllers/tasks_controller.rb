69d3a96c64a0d0f20490a9cde855f8433b096aa5
Gauderic Diagambana <diagambana.gauderic@gmail.com>
2021-06-08 14:00:45 +0200

    I can view a task

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,7 +1,17 @@
 class TasksController < ApplicationController
   def index
+    @tasks = Task.all
   end
 
-  def all
+  def show
+    @task = Task.find(params[:id])
+  end
+
+  def done?
+    if @task.completed == true
+      puts 'This task is done'
+    else
+      puts 'This task is not done'
+    end
   end
 end
